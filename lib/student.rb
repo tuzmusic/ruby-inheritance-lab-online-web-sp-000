3b756ed8af602b379944d63bc74bab7144fc04ba
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-13 23:06:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student  < User
 
-  attr_reader @knowledge
+  # attr_reader @knowledge
 
   def initialize
     @knowledge = []
@@ -9,9 +9,9 @@ class Student  < User
   def learn
     Teacher.teach
   end
-  #
-  # def knowledge
-  #   @knowledge
-  # end
+
+  def knowledge
+    @knowledge
+  end
 
 end
